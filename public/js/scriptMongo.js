8c9fe71f8907d861f816e18b66b3a038d88636ea
sreedharashwin <s223955271@deakin.edu.au>
2023-12-02 18:42:02 +1100

    Adding database

--- public/js/scriptMongo.js
+++ public/js/scriptMongo.js
@@ -6,7 +6,7 @@ const addCards = (items) => {
                 '<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right">more_vert</i></span><p><a href="#"></a></p></div>'+
                 '<div class="card-reveal">'+
                 '<span class="card-title grey-text text-darken-4">'+item.subTitle+'<i class="material-icons right">close</i></span>'+
-                '<p class="card-text">'+item.description+'</p>'+
+                '<p class="card-text" style="color:blue">'+item.description+'</p>'+
                 '</div></div></div>';
         $("#card-section").append(itemToAppend)
     });
--- startServerWithMongo.js
+++ startServerWithMongo.js
@@ -4,10 +4,12 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb://localhost:27017";
 let port = process.env.port || 3000;
 let collection;
+var cors = require('cors'); 
+app.use(cors());
 
 app.use(express.static(__dirname + '/public'))
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({extended: true}));
 
 const client = new MongoClient(uri, {
     serverApi: {
@@ -22,7 +24,7 @@ async function runDBConnection() {
     try {
         await client.connect();
         collection = client.db().collection('Cat');
-        console.log(collection);
+        console.log("Cat collection is " + collection);
     } catch(ex) {
         console.error(ex);
     }
@@ -34,7 +36,9 @@ app.get('/', function (req,res) {
 
 app.get('/api/cats', (req,res) => {
     getAllCats((err,result)=>{
+        console.log(err + " and " + result);
         if (!err) {
+            console.log(result);
             res.json({statusCode:200, data:result, message:'get all cats successful'});
         }
     });
@@ -46,15 +50,21 @@ app.post('/api/cat', (req,res)=>{
         if (!err) {
             res.json({statusCode:201, data:result, message:'success'});
         }
-    });
+    });  
 });
 
 function postCat(cat,callback) {
-    collection.insertOne(cat,callback);
+    collection.insertOne(cat,callback).then(function(res, err){
+        if(!err)
+            callback(err,res);
+    });
 }
 
 function getAllCats(callback){
-    collection.find({}).toArray(callback);
+    collection.find().toArray().then(function(res, err){
+        console.log(res);
+        callback(err,res);
+    });
 }
 
 app.listen(port, ()=>{
